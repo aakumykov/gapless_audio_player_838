a0d7f492e004bae9dc43e9eb5681ef0e89fd8190
Андрей Кумыков <aakumykov@yandex.ru>
2022-06-01 10:43:41 +0700

    iAudioPlayer.Callbacks --> GaplessPlayerCallbacks
    (cherry picked from commit 6ad2671448b20e699840f26bdaa9bdf8d22dd1e9)

---
+++ gapless_audio_player/src/main/java/com/gitlab/aakumykov/gapless_audio_player/GapplessPlayerCallbacks.java
@@ -0,0 +1,26 @@
+package com.gitlab.aakumykov.gapless_audio_player;
+
+import androidx.annotation.NonNull;
+
+public interface GapplessPlayerCallbacks {
+
+    void onStarted(@NonNull SoundItem soundItem);
+
+    void onStopped();
+
+    void onPaused();
+
+    void onResumed();
+
+    void onProgress(int position, int duration);
+
+    void onNoNextTracks();
+
+    void onNoPrevTracks();
+
+    void onNothingToPlay();
+
+    void onPreparingError(@NonNull SoundItem soundItem, @NonNull String errorMsg);
+
+    void onPlayingError(@NonNull SoundItem soundItem, @NonNull String errorMsg);
+}
